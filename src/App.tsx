ad119225e5d4ac019aadba021ccdc2e2fbb579be
psu6810110198 <6810110198@psu.ac.th>
2025-12-16 13:55:50 +0700

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import { useState } from 'react' // 1. Import useState
+import { useState } from 'react' 
 import './App.css'
 import NoteForm from './NoteForm'
 import NoteList from './NoteList'
-import { Note } from './types' // 2. Import Interface Note
-
+import { Note } from './types' 
 function App() {
-  // 3. ประกาศ State เก็บรายการ Note เป็น Array (<Note[]>)
+  
   const [notes, setNotes] = useState<Note[]>([])
 
-  // 4. สร้างฟังก์ชัน addNote รับ text เข้ามาสร้างเป็น Object Note ใหม่
+  
   const addNote = (text: string) => {
     const newNote: Note = {
-      id: Date.now(), // ใช้เวลาปัจจุบันเป็น ID ชั่วคราว
+      id: Date.now(), 
       text: text
     }
-    // อัปเดต State โดยเอา Note เดิม + Note ใหม่
+    
     setNotes([...notes, newNote])
-    console.log("Added:", newNote) // (Optional) เช็คดูใน Console ว่าเข้าไหม
+    console.log("Added:", newNote) 
   }
 
   return (
     <div className="App">
       <h1>Sticky Notes</h1>
-      {/* ส่งฟังก์ชัน addNote ไปให้ NoteForm ใช้งานผ่าน Props ชื่อ onAdd */}
+    
       <NoteForm onAdd={addNote} />
       <hr />
       <NoteList />
--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import { Note } from './types' // อย่าลืม Import Type
+import { Note } from './types' 
 
-// 1. กำหนด Props ตามโจทย์
 interface NoteListProps {
   notes: Note[];
 }
 
-// 2. รับค่า notes เข้ามา
+
 const NoteList = ({ notes }: NoteListProps) => {
   return (
     <div style={{ marginTop: '20px' }}>
       <h3>My Notes</h3>
-      {/* 3. วนลูปแสดงผลข้อมูล */}
+      
       {notes.map((note) => (
         <div key={note.id} style={{ border: '1px solid #ccc', padding: '10px', marginBottom: '5px', borderRadius: '5px' }}>
           {note.text}
